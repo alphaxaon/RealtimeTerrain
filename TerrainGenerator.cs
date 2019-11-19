b202ff9045f092fac034bb99f25f0ed82367c4a4
Alex Ovsyannikov <xaonst@gmail.com>
2019-11-19 10:40:00 -0600

    Clean up generation script

--- TerrainGenerator.cs
+++ TerrainGenerator.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 using System.Collections;
 using System.Linq;
 
-public class TerrainGenerator : MonoBehaviour {
+/**
+ * Terrain Generator
+**/
+public class TerrainGenerator : MonoBehaviour 
+{
+    /**
+     * Setup variables
+    **/
+    Terrain terrain;
+    string status = "";
 
+    /**
+     * Customizable parameters
+    **/
 	public int terrainWidth = 1000;
 	public int terrainHeight = 100;
 	public int terrainLength = 1000;
@@ -14,28 +26,23 @@ public class TerrainGenerator : MonoBehaviour {
 	public float lacunarity = 2;
 	public int seed = 0;
     [Range(0, 1)]
-    public float treeDensity;
-    public int distanceBetweenTrees = 1;
-	public Vector2 offset = new Vector2(0, 0);
-	public GameObject RandomTree;
-	//public GameObject particles;
-	public GameObject[] rocks;
-	Terrain terrain;
-    string status = "";
-    bool showStatus = true;
-    GameObject player;
-    Rigidbody playerBody;
+    public bool showStatus = false;
 
-    public IEnumerator GenerateTerrain() {
+    /**
+     * Generate the terrain
+    **/
+    public IEnumerator GenerateTerrain() 
+    {
+		terrain = GetComponent<Terrain>();
 
-		terrain = GetComponent<Terrain> ();
 		if (!terrain) {
-			Debug.Log ("Generate failed. No Terrain Component attached to this GameObject.");
+            status = "Generate failed. No Terrain Component attached to this GameObject.";
+			Debug.Log (status);
+            return false;
 		}
 
-        status = "Resetting terrain";
-
         // Set terrain size
+        status = "Resetting terrain";
         TerrainData terrain_data = terrain.terrainData;
         terrain_data.size = new Vector3(terrainWidth, terrainHeight, terrainLength);
         yield return null;
@@ -48,45 +55,40 @@ public class TerrainGenerator : MonoBehaviour {
         terrain.Flush();
         yield return null;
 
-        // Setup player
-        player = GameObject.FindGameObjectWithTag("Player");
-        PlacePlayerOnGround();
-
+        // Generate new random seed value
         status = "Generating terrain";
         yield return new WaitForSeconds(2f);
-
-        // Generate new random seed value
         seed = Random.Range (0, 60000);
 
+        // Generate features
         yield return StartCoroutine(GenerateShape(terrain_data));
         yield return StartCoroutine(GenerateCanyons(terrain_data));
         yield return StartCoroutine(GenerateErosion(terrain_data));
 
+        // Finish up
         status = "Terrain generated";
         yield return new WaitForSeconds(2f);
-        //showStatus = false;
         terrain_data = null;
     }
 
-	public IEnumerator GenerateShape(TerrainData terrain_data) {
-
+    /**
+     * Generate a noisemap to use as a heightmap for our terrain
+    **/
+	public IEnumerator GenerateShape(TerrainData terrain_data) 
+    {
         float[,] noiseMap = Noise.GenerateMap(terrain_data.heightmapWidth, terrain_data.heightmapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
         float[,] heights = terrain_data.GetHeights(0, 0, terrain_data.heightmapWidth, terrain_data.heightmapHeight);
         int t = 20;
 
-        for (int y = 0; y < terrain_data.heightmapHeight; y++)
-        {
-            for (int x = 0; x < terrain_data.heightmapWidth; x++)
-            {
+        for (int y = 0; y < terrain_data.heightmapHeight; y++) {
+            for (int x = 0; x < terrain_data.heightmapWidth; x++) {
                 float currentHeight = noiseMap[x, y];
                 heights[y, x] = currentHeight;
             }
             t--;
-            if (t <= 0)
-            {
+            if (t <= 0) {
                 status = "Terraforming land " + y + "/" + terrain_data.heightmapHeight;
                 terrain_data.SetHeights(0, 0, heights);
-                PlacePlayerOnGround();
                 t = 20;
                 yield return null;
             }
@@ -96,15 +98,17 @@ public class TerrainGenerator : MonoBehaviour {
         terrain.Flush();
     }
 
-	public IEnumerator GenerateCanyons(TerrainData terrain_data) {
-
+    /**
+     * Generate reverse heightmap to create canyons
+    **/
+	public IEnumerator GenerateCanyons(TerrainData terrain_data) 
+    {
 		float[,] noiseMap = Noise.GenerateMap (terrain_data.heightmapWidth, terrain_data.heightmapHeight, seed + 1, noiseScale/2, octaves, persistance, lacunarity + 1, offset);
 		float[,] heights = terrain_data.GetHeights (0, 0, terrain_data.heightmapWidth, terrain_data.heightmapHeight);
         int t = 20;
 
 		for (int y = 0; y < terrain_data.heightmapHeight; y++) {
 			for (int x = 0; x < terrain_data.heightmapWidth; x++) {
-
 				float currentHeight = noiseMap [x, y];
 
 				if (currentHeight < 0.5f)
@@ -115,25 +119,21 @@ public class TerrainGenerator : MonoBehaviour {
             {
                 status = "Generating canyons " + y + "/" + terrain_data.heightmapHeight;
                 terrain_data.SetHeights(0, 0, heights);
-                PlacePlayerOnGround();
                 t = 20;
                 yield return null;
             }
-            
         }
 
         terrain_data.SetHeights(0, 0, heights);
         terrain.Flush();
 
+        // Add more variation
         noiseMap = Noise.GenerateMap(terrain_data.heightmapWidth, terrain_data.heightmapHeight, seed + 2, noiseScale / 2, octaves + 1, persistance / 2, lacunarity, offset);
         heights = terrain_data.GetHeights(0, 0, terrain_data.heightmapWidth, terrain_data.heightmapHeight);
         t = 20;
 
-        for (int y = 0; y < terrain_data.heightmapHeight; y++)
-        {
-            for (int x = 0; x < terrain_data.heightmapWidth; x++)
-            {
-
+        for (int y = 0; y < terrain_data.heightmapHeight; y++) {
+            for (int x = 0; x < terrain_data.heightmapWidth; x++) {
                 float currentHeight = noiseMap[x, y];
 
                 if (currentHeight > 0.5f)
@@ -148,25 +148,23 @@ public class TerrainGenerator : MonoBehaviour {
                 t = 20;
                 yield return null;
             }
-
         }
 
         terrain_data.SetHeights(0, 0, heights);
         terrain.Flush();
     }
 
+    /**
+     * Generate finer noise to add detail
+    **/
     public IEnumerator GenerateErosion(TerrainData terrain_data)
     {
-
         float[,] noiseMap = Noise.GenerateMap(terrain_data.heightmapWidth, terrain_data.heightmapHeight, seed + 3, noiseScale / 2, octaves + 1, persistance / 2, lacunarity * 4, offset);
         float[,] heights = terrain_data.GetHeights(0, 0, terrain_data.heightmapWidth, terrain_data.heightmapHeight);
         int t = 20;
 
-        for (int y = 0; y < terrain_data.heightmapHeight; y++)
-        {
-            for (int x = 0; x < terrain_data.heightmapWidth; x++)
-            {
-
+        for (int y = 0; y < terrain_data.heightmapHeight; y++) {
+            for (int x = 0; x < terrain_data.heightmapWidth; x++) {
                 float currentHeight = noiseMap[x, y];
 
                 if (currentHeight > 0.0f && currentHeight < 0.4f)
@@ -181,15 +179,16 @@ public class TerrainGenerator : MonoBehaviour {
                 t = 20;
                 yield return null;
             }
-
         }
 
         terrain_data.SetHeights(0, 0, heights);
         terrain.Flush();
     }
 
+    /**
+     * Set limits on customizable values
+    **/
     void OnValidate() {
-
 		if (terrainWidth < 1)
 			terrainWidth = 1;
 
@@ -203,10 +202,12 @@ public class TerrainGenerator : MonoBehaviour {
 			octaves = 0;
 	}
 
+    /**
+     * Display generation status
+    **/
     void OnGUI()
     {
         if (showStatus)
             GUI.Label(new Rect(10, 10, 300, 50), status);
-
     }
 }
