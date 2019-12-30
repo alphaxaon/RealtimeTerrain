56e495ffe03506ac79da3abbc3c57bebd09dec3e
Alex Ovsyannikov <xaonst@gmail.com>
2019-12-30 10:57:41 -0600

    Add Generate Terrain button in Editor

---
+++ TerrainEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(TerrainGenerator))]
+
+public class TerrainEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        TerrainGenerator terrainGenerator = (TerrainGenerator)target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Generate Terrain"))
+        {
+            terrainGenerator.GenerateTerrain();
+        }
+    }
+}
